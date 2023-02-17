6bbdf429a79f2884d8c627e723ad3241b67b5440
Levi Raoelina <levi_r34@yahoo.com>
2023-02-17 14:24:35 -0700

    First assessment

---
+++ gitInfo.js
@@ -0,0 +1,78 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+let gitDefinition = "Git is a version control program."
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+let gitHubDefinition = " Git hub is a site where you can upload and store your code."
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+let gitInitDefinition = "First step to initialize code to store."
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+let gitCloneDefinition = "Can clone copies from server."
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+let gitStatusDefinition = "gives status of Git in the terminal"
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+let gitAddDefinition = "Adds file to staging area"
+let gitAddCode = "git add ."
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+let gitCommitDefinition = "Commits code to a repository in github along with a message from the user"
+let gitCommitCode = 'git commit -m "first commit"'
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+let gitPushDefinition = "git push pushes your code to the repository created in github."
---
+++ sdlc.js
@@ -0,0 +1,30 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintenence 
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysis = "This is the second step, in this step the developer needs to research what is asked of him and determine what needs to be done to complete the task."
+
+const design = "Step 3, the developer knows what he needs to do and now needs to come up with designs on how he wants to create the project."
+
+const Implementation = "Step 4, the developer is now done with planning, analysis, and design. Now the thoughts and ideas need to be written into code to make the project come together."
+
+const testing = "Step 5, after implementation, the developer needs to see if his ideas and code actually work. Thats where testing and debugging code come into play."
+
+const Maintenence = "Final step, the developer knows his code works, and has uploaded the project. Now he needs to check back regularly and maintain the site."
